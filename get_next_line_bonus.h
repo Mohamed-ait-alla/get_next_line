27825ed3f61d288473efa9f5ea0fb0cb8601f990
Mohamed Ait Alla <mait-all@e2r11p10.1337.ma>
2024-11-11 16:20:28 +0100

    Add Bonus part

---
+++ get_next_line_bonus.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mait-all <mait-all@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 10:53:13 by mait-all          #+#    #+#             */
+/*   Updated: 2024/11/11 16:18:26 by mait-all         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+# define MAX_FD 256
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h> // we don't neet this header in git 
+# include <fcntl.h> // we don't neet this header in git
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *s1);
+#endif
